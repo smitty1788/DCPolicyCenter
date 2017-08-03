64a9e59b00cd30eaabf115f8c27a65c806cc704e
Randy Smith <smithr@hood.edu>
2017-08-03 10:48:55 -0400

    Code refactoring, cleaning, and adding data

--- May 2017/Building_Carbon_Emissions/Analysis/building_emissions.py
+++ May 2017/Building_Carbon_Emissions/Analysis/building_emissions.py
@@ -6,14 +6,13 @@
 # Import arcpy module
 import arcpy
 
-
 # Local variables:
-Building_Footprints = "Building_Footprints"
-Common_Ownership_Lots = "Common_Ownership_Lots"
-emissions_point_data = "emissions_point_data"
-common_plot = "G:\\DC Policy Center\\Carbon Emmisions\\Data\\Spatial\\Carbon.gdb\\common_plot"
-emisz = "G:\\DC Policy Center\\Carbon Emmisions\\Data\\Spatial\\Carbon.gdb\\emisz"
-CO2_Emit_Buildings_shp = "G:\\DC Policy Center\\Carbon Emmisions\\Data\\Spatial\\Web\\Buildings\\CO2_Emit_Buildings.shp"
+Building_Footprints = "~\Carbon Emmisions\Data\Spatial\Building_Footprints\Building_Footprints.shp"
+Common_Ownership_Lots = "~\Carbon Emmisions\Data\Spatial\Common_Ownership_Lots\Common_Ownership_Lots.shp"
+emissions_point_data = "~\Carbon Emmisions\Data\Spatial\Carbon.gdb\emissions"
+common_plot = "~\Carbon Emmisions\Data\Spatial\Carbon.gdb\common_plot"
+building_co2 = "~\Carbon Emmisions\Data\Spatial\Carbon.gdb\building_co2"
+CO2_Emit_Buildings_shp = "~\Carbon Emmisions\Data\Spatial\Web\Buildings\CO2_Emit_Buildings.shp"
 
 # Spatial Join Building points to common plots
 arcpy.SpatialJoin_analysis(Common_Ownership_Lots, emissions_point_data, common_plot, 
@@ -25,19 +24,18 @@ Common_Ownership_Lots,OBJECTID,-1,-1;PROPERTYNAME \"PROPERTYNAME\" true true fal
 -1;KGsqft \"KGsqft\" true true false 8 Double 0 0 ,Sum,#,emissions,KGsqft,-1,-1", "INTERSECT", "", "")
 
 # Spatial Join common plots to building footprints
-arcpy.SpatialJoin_analysis(Building_Footprints, common_plot, emisz, 
+arcpy.SpatialJoin_analysis(Building_Footprints, common_plot, building_co2, 
 "JOIN_ONE_TO_ONE", "KEEP_ALL", "OBJECTID \"OBJECTID\" true true false 10 Long 0 10 ,First
 ,#,Building_Footprints,OBJECTID,-1,-1;PROPERTYNAME \"PROPERTYNAME\" true true false 8000 Text 0 0 ,First,#,
-G:\\DC Policy Center\\Carbon Emmisions\\Data\\Spatial\\Carbon.gdb\\common_plot,PROPERTYNAME,-1,-1;OWNEROFRECORD \"OWNEROFRECORD\" true true false 8000 Text 0 0 ,
-First,#,G:\\DC Policy Center\\Carbon Emmisions\\Data\\Spatial\\Carbon.gdb\\common_plot,OWNEROFRECORD,-1,-1;YEARBUILT \"YEARBUILT\" true true false 8000 Text 0 0 ,
-First,#,G:\\DC Policy Center\\Carbon Emmisions\\Data\\Spatial\\Carbon.gdb\\common_plot,YEARBUILT,-1,-1;ENERGYSTARSCORE \"ENERGYSTARSCORE\" true true false 8000 Text 0 0 ,First,#,
-G:\\DC Policy Center\\Carbon Emmisions\\Data\\Spatial\\Carbon.gdb\\common_plot,ENERGYSTARSCORE,-1,-1;Metrictons \"Metrictons\" true true false 8 Double 0 0 ,First,#,
-G:\\DC Policy Center\\Carbon Emmisions\\Data\\Spatial\\Carbon.gdb\\common_plot,Metrictons,-1,-1;KGsqft \"KGsqft\" true true false 8 Double 0 0 ,First,#,
-G:\\DC Policy Center\\Carbon Emmisions\\Data\\Spatial\\Carbon.gdb\\common_plot,KGsqft,-1,-1", "INTERSECT", "", "")
+~\Carbon Emmisions\Data\Spatial\Carbon.gdb\common_plot,PROPERTYNAME,-1,-1;OWNEROFRECORD \"OWNEROFRECORD\" true true false 8000 Text 0 0 ,
+First,#,~Carbon Emmisions\Data\Spatial\Carbon.gdb\common_plot,OWNEROFRECORD,-1,-1;YEARBUILT \"YEARBUILT\" true true false 8000 Text 0 0 ,
+First,#,~Carbon Emmisions\Data\Spatial\Carbon.gdb\common_plot,YEARBUILT,-1,-1;ENERGYSTARSCORE \"ENERGYSTARSCORE\" true true false 8000 Text 0 0 ,First,#,
+~\Carbon Emmisions\Data\Spatial\Carbon.gdb\common_plot,ENERGYSTARSCORE,-1,-1;Metrictons \"Metrictons\" true true false 8 Double 0 0 ,First,#,
+~\Carbon Emmisions\Data\Spatial\Carbon.gdb\common_plot,Metrictons,-1,-1;KGsqft \"KGsqft\" true true false 8 Double 0 0 ,First,#,
+~\Carbon Emmisions\Data\Spatial\Carbon.gdb\common_plot,KGsqft,-1,-1", "INTERSECT", "", "")
 
 # Filter only buildings with emissions data
-arcpy.SelectLayerByAttribute_management(emisz__2_, "NEW_SELECTION", "Metrictons IS NOT NULL")
+arcpy.SelectLayerByAttribute_management(building_co2, "NEW_SELECTION", "Metrictons IS NOT NULL")
 
 # Export shapefile for webmap
-arcpy.CopyFeatures_management(emisz__3_, CO2_Emit_Buildings_shp, "", "0", "0", "0")
-
+arcpy.CopyFeatures_management(building_co2, CO2_Emit_Buildings_shp, "", "0", "0", "0")
--- May 2017/Building_Carbon_Emissions/Analysis/emissions.R
+++ May 2017/Building_Carbon_Emissions/Analysis/emissions.R
@@ -8,107 +8,56 @@ library(lubridate)
 # Set working Directory
 setwd("G:/DC Policy Center/Carbon Emmisions/Data")
 
-
-
-# adding row for Howard University
-howard <- tribble(
-  ~PROPERTYNAME, ~REPORTINGYEAR, ~OWNEROFRECORD, ~REPORTEDADDRESS, ~YEARBUILT, ~PRIMARYPROPERTYTYPE_SELFSELECT, ~PRIMARYPROPERTYTYPE_EPACALC, ~TAXRECORDFLOORAREA, ~ENERGYSTARSCORE, ~TOTGHGEMISSIONS_METRICTONSCO2E, ~TOTGHGEMISSINTENSITY_KGCO2EFT, ~LATITUDE, ~LONGITUDE,
-  'Howard University', 2014, 'Howard University', NA, NA, 'College/University', 'College/University', NA, NA, 43301, 30, -77.020382, 38.921833)
-
 # Read in Data
-emissions <- read.csv("https://raw.githubusercontent.com/smitty1788/DCPolicyCenter/master/May%202017/Building_Carbon_Emissions/Analysis/Building_Energy_Benchmarks.csv", 
-                      stringsAsFactors = FALSE) %>% 
-  filter(REPORTINGYEAR == 2014) %>%
-  mutate(TOTGHGEMISSIONS_METRICTONSCO2E = as.numeric(TOTGHGEMISSIONS_METRICTONSCO2E),
-         TOTGHGEMISSINTENSITY_KGCO2EFT = as.numeric(TOTGHGEMISSINTENSITY_KGCO2EFT)) %>% 
-  rbind(howard)
-write.csv(emissions, 'Tab/building_emissions.csv', row.names = FALSE)
-  
-  
+# Original Dataset from 
+# http://opendata.dc.gov/datasets/2014-building-energy-and-water-performance-benchmarking
+# Due to inconsistent formating, much of the data cleanup
+# had to be done by hand 
+emissions <- read.csv('Tab/building_emissions.csv', stringsAsFactors = FALSE)
+
+# Addressing inconsistant naming schemes
+emissions$OWNEROFRECORD <- gsub("GEORGE WASHINGTON UNIVERSITY", "George Washington University", emissions$OWNEROFRECORD)
+emissions$OWNEROFRECORD <- gsub("GEORGE WASHINGTON", "George Washington University", emissions$OWNEROFRECORD)
+emissions$OWNEROFRECORD <- gsub("THE GEORGE WASHINGTON UNIVERSITY", "George Washington University", emissions$OWNEROFRECORD)
+emissions$OWNEROFRECORD <- gsub("THE George Washington University", "George Washington University", emissions$OWNEROFRECORD)
+emissions$OWNEROFRECORD <- gsub("GEORGETOWN COLLEGE LAW CENTER", "GEORGETOWN UNIVERSITY", emissions$OWNEROFRECORD)
+emissions$OWNEROFRECORD <- gsub("JESUIT COMMUNITY AT GEORGETOWN UNIV INC", "GEORGETOWN UNIVERSITY", emissions$OWNEROFRECORD)
 
 
-#-------------------------------------------------------------------------------------------------------------------------
-# Summarise CO2 emissions by type of building
-
-# Reduce number of property types to 10. Rename as "Other" if not in the top ten for count
-type <- emissions %>% 
-  group_by(PRIMARYPROPERTYTYPE_EPACALC) %>% 
-  count(PRIMARYPROPERTYTYPE_EPACALC) %>% 
-  arrange(desc(n)) %>%  
-  mutate(Property_Type = if_else(n < 6, "Other", PRIMARYPROPERTYTYPE_EPACALC),
-         Property_Type = if_else(Property_Type == "Residence Hall/Dormitory", "Other", Property_Type))
-
-# Table join new property types back to original table
-emit <- emissions %>% 
-  merge(y = type[ , c("PRIMARYPROPERTYTYPE_EPACALC", "Property_Type")], by = "PRIMARYPROPERTYTYPE_EPACALC", all.x = TRUE)
-
-
-# summarise by type
-type_summary <- emit %>% 
-  group_by(Property_Type) %>% 
-  summarise(Count = n(),
-            AvgCO2Tons = mean(TOTGHGEMISSIONS_METRICTONSCO2E, na.rm = TRUE),
-            AvgCO2KGSQFT = mean(TOTGHGEMISSINTENSITY_KGCO2EFT,na.rm = TRUE)) %>% 
-  arrange(desc(AvgCO2Tons))
-
-write.csv(type_summary, "Tab/Tidy/type_summary.csv", row.names = FALSE)
-
-ggplot(type_summary, aes(x = Property_Type, y = AvgCO2Tons, fill = Property_Type)) +
-  geom_bar(stat = "identity")
-  ggtitle("Vroom") +
-  scale_color_manual(values = pal("few_medium"))
-
-  
-
-#-------------------------------------------------------------------------------------------------------------------------
-# Summarise CO2 emissions by year built
-# Bin to 20 year intervals and summarise
-built <- emissions %>% 
-  filter(YEARBUILT >= 1860) %>% 
-  mutate(year_bucket = cut(YEARBUILT, breaks = c(1860, 1880, 1900, 1920, 1940, 1960, 1980, 2000, 2020), right = FALSE)) %>% 
-  group_by(year_bucket) %>% 
-  summarise(count = n(),
-            AvgCO2Tons = mean(TOTGHGEMISSIONS_METRICTONSCO2E, na.rm = TRUE),
-            AvgCO2KGSQFT = mean(TOTGHGEMISSINTENSITY_KGCO2EFT, na.rm = TRUE)) %>% 
-  arrange(desc(AvgCO2Tons))
-
-write.csv(built, "Tab/Tidy/year_summary.csv", row.names = FALSE)
-
-#-------------------------------------------------------------------------------------------------------------------------
-# List of top properties for emissions
-
-# Excluding GWU as they are calculate by building, will add in total sum for GWU below
-top <- emissions %>% 
-  filter(OWNEROFRECORD != "GEORGE WASHINGTON UNIVERSITY",
-         OWNEROFRECORD != "GALLAUDET UNIVERSITY") %>%
-  mutate(Property_Name = PROPERTYNAME,
-         Metric_Tons_CO2 = as.numeric(TOTGHGEMISSIONS_METRICTONSCO2E),
-         KGSQFT_CO2 = as.numeric(TOTGHGEMISSINTENSITY_KGCO2EFT)) %>% 
-  select(OWNEROFRECORD, Property_Name:KGSQFT_CO2)
-
-# Aggregating total sum for GWU and Galludet properties (Consistent with other Universities)
-gwu <- emissions %>% 
-  filter(OWNEROFRECORD == "GEORGE WASHINGTON UNIVERSITY") %>% 
-  group_by(OWNEROFRECORD) %>% 
-  summarise(Metric_Tons_CO2 = sum(TOTGHGEMISSIONS_METRICTONSCO2E, na.rm = TRUE), 
+# Top emitting properties
+top_prop <- emissions %>%
+  rename(Owner = OWNEROFRECORD,
+         Type = PRIMARYPROPERTYTYPE_SELFSELECT) %>%
+  mutate(Type = if_else(Owner %in% c('George Washington University', 'GEORGETOWN UNIVERSITY'), 'College/University', Type),
+         Owner = if_else(PROPERTYNAME == "Children's National Medical Center", "Children's National Medical Center", Owner)) %>% 
+  group_by(Owner, Type) %>% 
+  summarise(Metric_Tons_CO2 = sum(TOTGHGEMISSIONS_METRICTONSCO2E, na.rm = TRUE),
             KGSQFT_CO2 = sum(TOTGHGEMISSINTENSITY_KGCO2EFT, na.rm = TRUE)) %>% 
-  mutate(PROPERTYNAME = "GEORGE WASHINGTON UNIVERSITY",
-         Property_Name = PROPERTYNAME) %>% 
-  select(OWNEROFRECORD, Property_Name, Metric_Tons_CO2, KGSQFT_CO2)
-
-gall <- emissions %>% 
-  filter(OWNEROFRECORD == "GALLAUDET UNIVERSITY") %>% 
-  group_by(OWNEROFRECORD) %>% 
-  summarise(Metric_Tons_CO2 = sum(TOTGHGEMISSIONS_METRICTONSCO2E, na.rm = TRUE), 
-            KGSQFT_CO2 = sum(TOTGHGEMISSINTENSITY_KGCO2EFT, na.rm = TRUE)) %>% 
-  mutate(PROPERTYNAME = "Gallaudet University",
-         Property_Name = PROPERTYNAME) %>% 
-  select(OWNEROFRECORD, Property_Name, Metric_Tons_CO2, KGSQFT_CO2)
-
-# Binding GWU/Galludet back with original list
-top <- top %>% 
-  rbind(gwu, gall) %>% 
   arrange(desc(Metric_Tons_CO2))
-top$Property_Name <- gsub("GEORGE WASHINGTON UNIVERSITY", "George Washington University", top$Property_Name)
-write.csv(top, "Tab/Tidy/property_summary.csv", row.names = FALSE)
+
+
+# Top emmissions by College/University
+colleges <- top_prop %>% 
+  filter(Type == 'College/University') %>% 
+  arrange(desc(KGSQFT_CO2))
+
+# Enrollment Data
+# Data gathered from each universities respective wikipedia page
+uni_names <- sort(unique(colleges$Owner))
+
+uni_enrollment <- tibble(
+  Owner = uni_names,
+  enrollment = c(13200, 2340, 27159, 17849, 10300, NA, NA, 6521, NA, NA, 2100)
+)
+
+# College emissions by enrollment
+college_emissions <- colleges %>% 
+  merge(uni_enrollment, by = 'Owner') %>% 
+  mutate(CO2_Enrollment = round(Metric_Tons_CO2 / enrollment, 1)) %>% 
+  arrange(desc(CO2_Enrollment)) %>% 
+  filter(!is.na(CO2_Enrollment))
+
+# write data to csv
+write.csv(top_prop, 'Tab/Tidy/top_emissions.csv', row.names = FALSE)
+write.csv(college_emissions, 'Tab/Tidy/college_emissions.csv', row.names = FALSE)
 
